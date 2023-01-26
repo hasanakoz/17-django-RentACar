ddcc66cb02bb3262ea036556d956f5437a8ddbc3
hasan <hasanakoz1986@gmail.com>
2023-01-26 22:27:20 +0300

    car serializer, permissions and view done

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import CarSerializer
+
+from .models import Car, Reservation
+from .permissions import IsStaffOrReadOnly
 
 # Create your views here.
+
+
+class CarView(ModelViewSet):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+    permission_classes = [IsStaffOrReadOnly,]
